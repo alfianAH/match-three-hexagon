b3993748aba1ea321d4c1b7d0a1e45cff6cc3a1e
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-05-09 17:24:43 +0800

    Select the tile

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -6,11 +6,44 @@ public class TileController : MonoBehaviour
 
     private BoardManager board;
     private SpriteRenderer spriteRenderer;
+    
+    private static readonly Color SelectedColor = new Color(0.5f, 0.5f, 0.5f);
+    private static readonly Color NormalColor = Color.white;
+
+    private static TileController previousSelected;
+
+    private bool isSelected;
 
     private void Awake()
     {
         board = BoardManager.Instance;
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        // Non selectible condition
+        if (spriteRenderer.sprite == null) return;
+        
+        // When mouse is clicked on the tile
+        // If tile is selected, ...
+        if (isSelected)
+        {
+            Deselect(); // Deselect the tile
+        }
+        else 
+        {
+            // If nothing is selected yet, ...
+            if (previousSelected == null)
+            {
+                Select(); // Select the tile
+            }
+            else  // If there is one, ...
+            {
+                previousSelected.Deselect(); // Deselect the previous one
+                Select(); // Select the new one
+            }
+        }
     }
 
     /// <summary>
@@ -25,4 +58,28 @@ public class TileController : MonoBehaviour
         this.id = id;
         name = $"TILE_{id}({x},{y})";
     }
+
+    #region Select & Deselect
+
+    /// <summary>
+    /// Tile's condition when it's selected
+    /// </summary>
+    private void Select()
+    {
+        isSelected = true;
+        spriteRenderer.color = SelectedColor;
+        previousSelected = this;
+    }
+
+    /// <summary>
+    /// Tile's condition when it's deselected
+    /// </summary>
+    private void Deselect()
+    {
+        isSelected = false;
+        spriteRenderer.color = NormalColor;
+        previousSelected = null;
+    }
+
+    #endregion
 }
